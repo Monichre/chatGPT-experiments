7249871ef172a069cecae66d062036b465f66439
monichre <liamhellis@gmail.com>
2023-01-10 00:08:06 -0500

    tweaks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,23 @@
-import { Layout, Text, Page } from '@vercel/examples-ui'
-import { Chat } from '../components/Chat'
+import { Layout, Page, Text } from '@vercel/examples-ui';
+
+import { Chat } from '../components/Chat';
 
 function Home() {
   return (
-    <Page className="flex flex-col gap-12">
-      <section className="flex flex-col gap-6">
-        <Text variant="h1">OpenAI GPT-3 text model usage example</Text>
-        <Text className="text-zinc-600">
-          In this example, a simple chat bot is implemented using Next.js, API
-          Routes, and OpenAI API.
+    <Page className='flex flex-col gap-12'>
+      <section className='flex flex-col gap-6'>
+        <Text variant='h1'>Liam's OpenAI GPT-3 Experiments</Text>
+        <Text>
+          <b>Conversational Chatbot (Q&A)</b>
+        </Text>
+        <Text className='text-zinc-600'>
+          Technologies: Next.js, API Routes, and OpenAI API.
         </Text>
       </section>
 
-      <section className="flex flex-col gap-3">
-        <Text variant="h2">AI Chat Bot:</Text>
-        <div className="lg:w-2/3">
+      <section className='flex flex-col gap-3'>
+        <Text variant='h2'>Talk to me baby</Text>
+        <div className='lg:w-2/3'>
           <Chat />
         </div>
       </section>
