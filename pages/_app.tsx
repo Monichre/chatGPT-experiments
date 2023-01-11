5404b3bbcc6eef593fa6b534817d05d848c39804
monichre <liamhellis@gmail.com>
2023-01-10 20:31:21 -0500

    removed Layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,9 @@
 import '@vercel/examples-ui/globals.css';
 
-import { Analytics } from '@vercel/analytics/react';
-import { getLayout } from '@vercel/examples-ui';
-
 import type { AppProps } from 'next/app'
-import type { LayoutProps } from '@vercel/examples-ui/layout'
-function App({ Component, pageProps }: AppProps) {
-  const Layout = getLayout<LayoutProps>(Component)
-  console.log('Layout: ', Layout)
 
-  return (
-    <Layout
-      title="Liam's AI bot"
-      path='/ai-chatgpt'
-      description='Powered by ChatGPT'
-    >
-      <Component {...pageProps} />
-      <Analytics />
-    </Layout>
-  )
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
 
 export default App
